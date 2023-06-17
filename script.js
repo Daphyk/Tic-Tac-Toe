3586462c737fb35a7cae01b99c954f16cf4e04cc
Doreen Kendi Thuranira <kendid07@gmail.com>
2023-06-18 01:54:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ document.addEventListener('DOMContentLoaded', () => {
 let playerText = documnet.getElementById("playerText");
 let restartBtn = document.getElementById("restartBtn");
 let boxes = Array.from(document.getElementsByClassName("box");
+
+ 
 let winnerIndicator = getComputeredStyle(document.body).getPropertyValue('--winning-blocks')
 const o_TEXT = "o"
 const x_TEXT = "x"
