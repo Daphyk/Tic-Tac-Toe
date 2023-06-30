f298a66a7f73d6a3146af4980feaf5cdcb4df34a
Doreen Kendi Thuranira <kendid07@gmail.com>
2023-06-30 15:39:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -82,6 +82,7 @@ function restart() {
     let cardId = this.getAttribute('data-id')
     cardsChosen.push(cardArray[cardId].name)
     cardsChosenId.push(cardId)
+    zjnzz 
     this.setAttribute('src', cardArray[cardId].img)
     if (cardsChosen.length ===2) {
       setTimeout(checkForMatch, 500)
