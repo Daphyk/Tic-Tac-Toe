a5780804c55e6756168f0e1d7fd33ae7ca6ba06d
Doreen Kendi Thuranira <kendid07@gmail.com>
2023-07-04 23:36:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
  
 let playerText = documnet.getElementById("playerText");
+ 
 let restartBtn = document.getElementById("restartBtn");
  
 let boxes = Array.from(document.getElementsByClassName("box");
